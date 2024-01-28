f6b13cfed54c75aff6ec839c8572dac0bfca8d6e
Yogev Ofir <118372150+YogevOfir@users.noreply.github.com>
2024-01-28 18:19:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post('/api/signup', async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    const newUser = new User({ email, password: hashedPassword, type });
+    const newUser = new User({ email: email, password: hashedPassword, type: type });
     await newUser.save();
 
     res.status(201).json({ message: 'User created successfully' });
