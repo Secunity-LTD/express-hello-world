c20c3522e456ce7a6725704edabb708c3d602eac
Yogev Ofir <118372150+YogevOfir@users.noreply.github.com>
2024-01-28 18:31:44 +0200

    Update user_model.js

--- models/user_model.js
+++ models/user_model.js
@@ -2,8 +2,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
+  email: { type: String, unique: true, required: true},
+  password: {type: String, required: true},
+  type: {type: String, required: true}
 });
 
 const User = mongoose.model('User', userSchema);
