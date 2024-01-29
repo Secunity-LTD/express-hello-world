5d400a8a0a69175627a1de01f0ca2cc3a2526fa8
Yogev Ofir <118372150+YogevOfir@users.noreply.github.com>
2024-01-29 13:12:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ app.post('/api/signup', async (req, res) => {
     const newUser = new User({ email: email, password: hashedPassword, type: type });
     await newUser.save();
 
-    res.status(201).json({ message: 'User created successfully' });
+    const user_type = user.type;
+    res.status(200).json({ message: 'Login successful', type: user_type});
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal Server Error' });
