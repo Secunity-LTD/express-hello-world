c42190a97a00b69ebf69f7bc29b1f36013c021e5
Yogev Ofir <yogev282000@gmail.com>
2024-02-04 15:57:53 +0200

    jil

--- models/team_model.js
+++ models/team_model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const User = require('./user_model');
 
 const teamSchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true},
+    name: { type: String, unique: true, required: true },
 });
 
 const Team = mongoose.model('Team', teamSchema);
